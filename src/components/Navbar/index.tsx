c79a43e240790f92f9d685cad6396fb8d394a83f
Joao Ladeira <joaoladeirag@gmail.com>
2022-12-22 20:09:11 -0300

    update routes on navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,20 @@
+import { AppRoutes } from '../../routes';
 import './index.css';
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <a href="/" className="logo">
+      <a href={AppRoutes.HOME} className="logo">
         <img src="/logo.png" alt="logo" />
       </a>
       <ul className="navbar-nav">
         <li>
-          <a href="/signup" className="nav-link">
+          <a href={AppRoutes.SIGNUP} className="nav-link">
             <span className="link-text">Criar Conta</span>
           </a>
         </li>
         <li className="nav-item">
-          <a href="/login" className="nav-link">
+          <a href={AppRoutes.SIGNIN} className="nav-link">
             <span className="link-text">Entrar</span>
           </a>
         </li>
--- src/routes/root/index.tsx
+++ src/routes/root/index.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { Navbar } from '../../components/Navbar';
 
 export const Root = () => {
-  // TODO: build navbar here
   return (
     <>
       <Navbar />
