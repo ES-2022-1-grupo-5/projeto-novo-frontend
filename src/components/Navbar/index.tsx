12b5cdf915cd03900048b7e980e01f607c78d9f1
Joao Ladeira <joaoladeirag@gmail.com>
2022-12-22 20:49:27 -0300

    refator(navbar): migrate to chakra ui

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,24 +1,58 @@
+import { Button, Flex, HStack, Image, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { AppRoutes } from '../../routes';
-import './index.css';
 
 export const Navbar = () => {
+  const location = useLocation();
+
+  if (
+    location.pathname === AppRoutes.SIGNUP ||
+    location.pathname === AppRoutes.SIGNIN
+  ) {
+    return (
+      <Flex
+        as="nav"
+        justifyContent="space-between"
+        alignItems="center"
+        px="1rem"
+        h="4rem"
+        bg={location.pathname === AppRoutes.SIGNUP ? '#8F2031' : '#262e57'}
+      >
+        <Link to={AppRoutes.ROOT}>
+          <Image src="/logo.png" alt="logo" />
+        </Link>
+
+        <HStack spacing="2rem">
+          <Link to={AppRoutes.ROOT}>
+            <Button colorScheme="orange">Precisa de ajuda?</Button>
+          </Link>
+        </HStack>
+      </Flex>
+    );
+  }
+
   return (
-    <nav className="navbar">
-      <a href={AppRoutes.ROOT} className="logo">
-        <img src="/logo.png" alt="logo" />
-      </a>
-      <ul className="navbar-nav">
-        <li>
-          <a href={AppRoutes.SIGNUP} className="nav-link">
-            <span className="link-text">Criar Conta</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href={AppRoutes.SIGNIN} className="nav-link">
-            <span className="link-text">Entrar</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <Flex
+      as="nav"
+      justifyContent="space-between"
+      alignItems="center"
+      px="1rem"
+      h="4rem"
+      bg="#262e57"
+    >
+      <Link to={AppRoutes.ROOT}>
+        <Image src="/logo.png" alt="logo" />
+      </Link>
+
+      <HStack spacing="2rem">
+        <Link to={AppRoutes.SIGNUP}>
+          <Text color="white">Criar Conta</Text>
+        </Link>
+        <Link to={AppRoutes.SIGNIN}>
+          <Button colorScheme="orange">Entrar</Button>
+        </Link>
+      </HStack>
+    </Flex>
   );
 };
