20adc2c15373dcb24f038b7e360a56f77821d3f3
Joao Ladeira <joaoladeirag@gmail.com>
2022-12-22 18:41:19 -0300

    styles(login): migrate css to chakraui

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,4 +1,12 @@
-import { Box, Input, Text, Button, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Input,
+  Text,
+  Button,
+  VStack,
+  Flex,
+  Image,
+} from '@chakra-ui/react';
 import LoginIllustration from '../../assets/illustrations/login-illustration.svg';
 import './index.css';
 
@@ -13,20 +21,37 @@ export const Login = () => {
   };
 
   return (
-    <main className="login">
-      <img src={LoginIllustration} className="background-illustration" />
-      <Box bg="#262E57" className="container">
+    <Flex as="main" direction="row-reverse" h="100%" px="6rem" py="4rem">
+      <Image
+        src={LoginIllustration}
+        position="absolute"
+        top="0"
+        left="0"
+        h="100%"
+        zIndex="-1"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+        backgroundPosition="left"
+      />
+      <Box bg="#262E57" p="3rem">
         <Text fontSize="3xl" color="white">
           Entre para encontrar seu próximo racha!
         </Text>
-        <form onSubmit={handleSubmit} className="form">
+        <form onSubmit={handleSubmit}>
           <VStack spacing="2">
-            <Input id="email" name="email" type="email" />
-            <Input type="password" />
-            <Button>Entrar</Button>
+            <Input
+              bg="white"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="email"
+            />
+            <Input bg="white" type="password" placeholder="senha" />
           </VStack>
+          <Text color="white">Esqueceu sua senha?</Text>
+          <Button>Entrar</Button>
         </form>
       </Box>
-    </main>
+    </Flex>
   );
 };
